196f696d002a65ae92242f0ca8e46463504ce062
deltasquare4 <deltasquare4@users.noreply.github.com>
2021-12-09 11:34:16 +0000

    deploy: Improwised/improwised-website@b2d3cee390811fed4f7d4f52e013c48fe46fcefa

---
+++ _nuxt/be11bce.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{252:function(n,t,e){"use strict";e.r(t);var l=e(74),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("Nuxt")}),[],!1,null,null,null);t.default=component.exports}}]);
+//# sourceMappingURL=be11bce.js.map
