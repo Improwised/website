84dc72e572ced24ca70fa7500b0e7576e2a6bda0
monikagorkhiya <monikagorkhiya@users.noreply.github.com>
2023-10-12 05:44:28 +0000

    deploy: Improwised/improwised-website@6c699ae5c89d2c17490b9df79ccc955edae9a571

--- _nuxt/static/1697032059/services/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697032059",layout:"theme",error:a,state:a,serverRendered:false,routePath:"\u002Fservices",config:{MODE:"production",BASE_URL:"https:\u002F\u002Fwww.improwised.com",DATA_URL:"https:\u002F\u002Fdata.improwised.com",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
