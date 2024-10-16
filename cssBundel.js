f06fbaf34cb2add7ec486ecc236a6cd68003160a
monikagorkhiya <monikagorkhiya@users.noreply.github.com>
2024-10-16 10:34:13 +0000

    deploy: Improwised/improwised-website@5047eebc3d562ab33d8a8199ea69211efeda07c4

--- cssBundel.js
+++ cssBundel.js
@@ -1,10 +1,5 @@
 require("./theme/css/bootstrap.css");
-require("./theme/css/stack-interface.css");
-require("./theme/css/socicon.css");
-// require("./theme/css/lightbox.min.css");
 require("./theme/css/flickity.css");
-require("./theme/css/iconsmind.css");
-// require("./theme/css/jquery.steps.css");
 require("./theme/css/theme.css");
 require("./css/custom.css");
 require("./css/pages.css");
